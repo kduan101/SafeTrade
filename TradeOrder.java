177503c43f7b182b75e50ce5f557a47145ead86a
kduan101 <39505688+kduan101@users.noreply.github.com>
2018-12-10 11:55:56 -0500

    Add files via upload

---
+++ TradeOrder.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TradeOrder {
+	
+	public TradeOrder(Trader trader, String string, boolean buyOrder, 
+			boolean marketOrder, int numShares, double price) {
+		
+		
+	}
+
+}
